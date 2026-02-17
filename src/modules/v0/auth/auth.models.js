0746435036d76e6e318813867a7a1b47b19f2c19
Denylson <denylsongpereira@gmail.com>
2026-02-17 21:10:19 +0000

    feat(auth): Add login endpoint and whoami endpoint

--- src/modules/v0/auth/auth.models.js
+++ src/modules/v0/auth/auth.models.js
@@ -12,6 +12,28 @@ const userSchema = new mongoose.Schema({
     },
 });
 
-const User = mongoose.model('User', userSchema);
+export const User = mongoose.model('User', userSchema);
 
-export default User;
+
+const refreshTokenSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+    },
+    expiresAt: {
+        type: Date,
+        required: true,
+    },
+    state: {
+        type: String,
+        enum: ['active', 'revoked'],
+        default: 'active',
+    },
+});
+
+export const RefreshToken = mongoose.model('RefreshToken', refreshTokenSchema);
--- src/modules/v0/auth/auth.service.js
+++ src/modules/v0/auth/auth.service.js
@@ -1,10 +1,11 @@
-import User from './auth.models.js';
+import jwt from 'jsonwebtoken';
+import { User, RefreshToken } from './auth.models.js';
 import bcrypt from 'bcryptjs';
 
 export class AuthService {
 
     /**
-     * Registers a new user in the system.
+     * Registers a new user
      * @param {Object} data - An object containing user details (name, password).
      * @returns {Promise<User>}
      */
@@ -22,7 +23,47 @@ export class AuthService {
         };
 
         const user = await User.create(userData);
-        const userSafe = { id: user._id, name: user.name };
-        return { status: 201, user: userSafe };
+        const pubUser = { id: user._id, name: user.name };
+        return { status: 201, user: pubUser };
+    }
+
+    /**
+     * Log in a user and generate access and refresh tokens
+     * @param {Object} data - An object containing user details (name, password).
+     * @returns {Promise<{error: string, status: number} | {status: number, accessToken: string, refreshToken: string}>}
+     */
+    async login(data) {
+        const user = await User.findOne({name: data.name});
+
+        if (!user) return { error: 'Invalid name or password', status: 404 };
+
+        const isValid = await bcrypt.compare(data.password, user.password);
+        
+        if (!isValid) return { error: 'Invalid name or password', status: 404 };
+
+        const accessToken = await jwt.sign({ id: user._id, name: user.name }, process.env.JWT_SECRET, { expiresIn: '15m' });
+
+        const refreshToken = await jwt.sign({ id: user._id, name: user.name }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
+
+        await RefreshToken.create({
+            userId: user._id,
+            token: refreshToken,
+            expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        });
+
+        return { status: 200, accessToken, refreshToken };
+    }
+
+    /**
+     * Methods to get information about the currently authenticated user
+     * @param {String} id - The ID of the user to retrieve information for.
+     * @returns {Promise<{error: string, status: number} | {status: number, user: Object}>}
+     */
+    async whoami(id) {
+        const user = await User.findById(id);
+        if (!user) return { error: 'User not found', status: 404 };
+        
+        const pubUser = { id: user._id, name: user.name };
+        return { status: 200, user: pubUser };
     }
 }
